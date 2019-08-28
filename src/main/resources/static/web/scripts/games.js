6c0a3d7f3e54ceeeb530296a568db3787e5777e4
Claudio Pera <cgpera@gmail.com>
2019-08-28 12:09:53 -0300

    games.js limpieza de código

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -18,8 +18,8 @@ $(document).ready(function () {
       valid0 = val.gamePlayers[0]
       valid1 = val.gamePlayers[1]
       items.push("<div class='divider'></div><div class='section'>");
-      items.push("<h5>Section " + key + "</h5>")
-      items.push(`<p> Game ID: ${val.id} Game Date Creation: ${val.created}</p>`)
+      items.push("<h5 class='blue-text text-darken-2'>Game " + key + "</h5>")
+      items.push(`<p class="card-panel teal lighten-2"> Game ID: ${val.id} Game Date Creation: ${val.created}</p>`)
       items.push(`<p> User ID: ${valid0.player.id} User Name: ${valid0.player.userName}</p>`)
       items.push( `<p> User ID: ${valid1.player.id} User Name: ${valid1.player.userName}</p>`)
       items.push("</div>")
