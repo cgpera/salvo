180253cd11120bc2b97b4d1e8e1421372cbb17a7
Claudio Pera <cgpera@gmail.com>
2019-09-12 11:54:11 -0300

    incorporación menú scores

--- src/main/resources/static/web/js/game.js
+++ src/main/resources/static/web/js/game.js
@@ -16,12 +16,11 @@ function loadData() {
       else
         playerInfo = [data.gamePlayers[1].player, data.gamePlayers[0].player];
 
-      $('#playerInfo').text(playerInfo[0].userName + '(you) vs ' + playerInfo[1].userName);
-
+      $('#playerInfo').text(playerInfo[0].email + '(you) vs ' + playerInfo[1].email);
 
       data.ships.forEach(function (shipPiece) {
         shipPiece.locations.forEach(function (shipLocation) {
-          if(isHit(shipLocation,data.salvoes,playerInfo[0].id)  !==  0){
+          if(isHit(shipLocation,data.salvoes,playerInfo[0].id)  !=  0){
             $('#B_' + shipLocation).addClass('ship-piece-hited');
                         $('#B_' + shipLocation).text(isHit(shipLocation,data.salvoes,playerInfo[0].id));
           }
@@ -30,15 +29,10 @@ function loadData() {
             $('#B_' + shipLocation).addClass('ship-piece');
         });
       });
-      console.log(data.salvoes, data.salvoes[0].turn, data.salvoes[0].player)
       data.salvoes.forEach(function (salvo) {
-          turno = salvo.turn
         if (playerInfo[0].id === salvo.player) {
-            console.log(salvo.turn)
           salvo.locations.forEach(function (location) {
-//              console.log("locations", location.)
             $('#S_' + location).addClass('salvo-piece');
-              $('#S_' + location).text(turno);
           });
         } else {
           salvo.locations.forEach(function (location) {
@@ -55,10 +49,10 @@ function loadData() {
 function isHit(shipLocation,salvoes,playerId) {
   var turn = 0;
   salvoes.forEach(function (salvo) {
-    if(salvo.player !== playerId)
+    if(salvo.player != playerId)
       salvo.locations.forEach(function (location) {
         if(shipLocation === location)
-            turn = salvo.turn
+          turn = salvo.turn;
       });
   });
   return turn;
