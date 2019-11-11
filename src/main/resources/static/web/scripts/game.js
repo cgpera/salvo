5edbc2a99dc3b692915f3a90f6c2980e575e340a
Claudio Pera <cgpera@gmail.com>
2019-11-11 18:43:43 -0300

    game

--- src/main/resources/static/web/scripts/game.js
+++ src/main/resources/static/web/scripts/game.js
@@ -21,6 +21,7 @@ console.log(url)
       items.push("<h5 class='blue-text text-darken-2'>Game " + respuesta.id + "</h5>")
       items.push(`<p class="card-panel teal lighten-2"> Game ID: ${respuesta.id} Game Date Creation: ${respuesta.created}</p>`)
     console.log("game id", respuesta.id)
+    console.log("respuesta ", respuesta)
     console.log("game date creat", respuesta.created)
 
       valid0 = respuesta.gamePlayers[0]
@@ -28,6 +29,7 @@ console.log(url)
       items.push(`<p class="card-panel teal lighten-2"> Game ID: ${respuesta.id} Game Date Creation: ${respuesta.created}</p>`)
 
       console.log("user0", valid0)
+      console.log("name", valid0.player.userName, )
 //      if(valid0 !== null) {
         items.push(`<p> User ID: ${valid0.player.id} User Name: ${valid0.player.userName}</p>`)
 //      }
